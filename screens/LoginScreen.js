8974bae03b9c547505e5de279314603516e381fd
Rohan Udhwani <rohanudhwani2@gmail.com>
2023-12-27 21:29:11 +0530

    styling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,13 @@
 import { Image, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { cloneElement } from 'react'
+import React, { cloneElement, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {MaterialIcons, AntDesign} from '@expo/vector-icons'
 
 const LoginScreen = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white", alignItems: 'center' }} >
       <View style={{ marginTop: 50 }} >
@@ -18,18 +22,18 @@ const LoginScreen = () => {
         <View style={{ marginTop: 40 }}>
           <View style={{flexDirection:"row", alignItems:"center", gap:5, borderColor:"#D0D0D0", borderWidth:1, paddingVertical:5, borderRadius:5}}>
             <MaterialIcons style={{marginLeft:8}} name="email" size={24} color="gray" />
-            <TextInput placeholderTextColor={"gray"} style={{color:"gray", marginVertical:10, width:300}} placeholder="Enter your email" />
+            <TextInput value={email} onChangeText={(text) => setEmail(text)} placeholderTextColor={"gray"} style={{color:"gray", marginVertical:10, width:300, fontSize:password?16:16}} placeholder="Enter your email" />
           </View>
 
           <View style={{ marginTop: 30 }}>
             <View style={{flexDirection:"row", alignItems:"center", gap:5, borderColor:"#D0D0D0", borderWidth:1, paddingVertical:5, borderRadius:5}}>
               <AntDesign style={{marginLeft:8}} name="lock" size={24} color="gray" />
-              <TextInput placeholderTextColor={"gray"} style={{color:"gray", marginVertical:10, width:300}} placeholder="Enter your password" />
+              <TextInput value={password} onChangeText={(text) => setPassword(text)} placeholderTextColor={"gray"} style={{color:"gray", marginVertical:10, width:300, fontSize:password?16:16}} placeholder="Enter your password" />
             </View>
           </View>
 
-          <View>
-            <Text style={{flexDirection:'row', alignItems:"center", justifyContent:"space-between", marginTop:12}}>Keep me logged in</Text>
+          <View style={{flexDirection:'row', alignItems:"center", justifyContent:"space-between", marginTop:12}}>
+            <Text style={{fontWeight:"500", color:"#007FFF"}}>Keep me logged in</Text>
             <Text>Forgot Password?</Text>
           </View>
         </View>
